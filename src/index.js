442aeecffc6af4be66ab940d0610fd926ce1310b
Mangaliso <mangaliso.twala@gmail.com>
2021-12-25 19:53:24 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
